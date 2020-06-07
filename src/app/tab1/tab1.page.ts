4e7b3db01cf81eeb68324a0c84048119ff8c145d
biancademaria <bianca.maria@etec.sp.gov.br>
2020-06-07 16:14:37 -0300

    calculadora app
    não consegui fazer as contas ainda

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public primeiroValor: number;
+  public segundoValor: number;
 
+  public resultado : any;
+  
+
+  constructor() {
+
+  }
+
+  public soma() {
+    let resultado = (this.primeiroValor) + (this.segundoValor);
+    this.resultado = resultado;
+  }
+
+  public subtracao() {
+    let resultado = (this.primeiroValor) - (this.segundoValor);
+    this.resultado = resultado;
+  }
+
+  public multiplicacao() {
+    let resultado = (this.primeiroValor) * (this.segundoValor);
+    this.resultado = resultado;
+  }
+
+  public divisao() {
+    let resultado = (this.primeiroValor) / (this.segundoValor);
+    this.resultado = resultado;
+  }
+
+  public limpar() {
+    let primeiroValor = null;
+    let segundoValor = null;
+    let resultado = ("");
+    this.primeiroValor = primeiroValor;
+    this.segundoValor - segundoValor;
+    this.resultado = resultado; 
+
+  }
 }
+
